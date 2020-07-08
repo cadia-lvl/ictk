1ed7f9d699c9139fe32fed8f974fe487b2895e77
Egill Anton Hlöðversson <egillanton@users.noreply.github.com>
2020-07-08 10:22:38 +0000

    Update __init__.py

--- ictk/__init__.py
+++ ictk/__init__.py
@@ -1,5 +1,6 @@
+#!/usr/bin/env python
 """
-	ICTK: A collection of scripts to use with various Icelandic text corpora
-    The Icelandic Gigaword Corpus (IGC) module
-    Copyright (C) 2020 Mál- og raddtæknistofa Gervigreindarseturs HR - Language and Voice Lab
+	ICTK: A collection of scripts to use with various Icelandic text corpora 
+	The Icelandic Gigaword Corpus (IGC) module 
+	Copyright (C) 2020 Mál- og raddtæknistofa Gervigreindarseturs HR - Language and Voice Lab 
 """
